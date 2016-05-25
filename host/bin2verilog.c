8fb5d821cfc474924e9921c5b34df1e46b82b1e3
Dirk Möller <moeller.dirk80@gmail.com>
2016-05-25 21:14:20 +0200

    bits configurable

--- host/bin2verilog.c
+++ host/bin2verilog.c
@@ -1,21 +1,38 @@
 ﻿#include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
     if( argc < 2 ) {
-        printf("bin2verilog infile [outfile]\n");
+        fprintf(stderr, "bin2verilog infile [bitwidth]\n");
         return 1;
     }
     FILE *datei = fopen( argv[1], "r" );
     if( datei == NULL ) {
-        printf("Datei nicht gefunden.\n");
+        fprintf(stderr, "Datei nicht gefunden.\n");
         return 2;
     }
+
+	int width = 16;
+
+	if( argc == 3 ) {
+		width = strtoul( argv[2], NULL, 10 );
+	}
+	if( width % 8 != 0 ) {
+		fprintf( stderr, "Width not a multiple of 8\n" );
+		return 3;
+	}	
+
     uint32_t pos = 0;
-    unsigned char vals[2];
+    unsigned char vals[width/8];
+
     while( !feof(datei) ) {
         fread( vals, sizeof( vals ), 1, datei );
-        printf("24'h%06X: boot_read[15:0] = 16'h%02X%02X;\n", pos, vals[0], vals[1]);
+        printf("32'h%08X: boot_read[%d:0] = %d'h", pos, width-1, width);
+		for( int i = 0; i < width/8; i++ ) {
+			printf("%02X", vals[i] );
+		}
+		printf(";\n");
         pos+=sizeof( vals );
     }
     return 0;
